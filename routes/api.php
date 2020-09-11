b29458e257f33b9f7849fd3a8439626cabeb5048
denizsetyawan <denizsetyawan95@gmail.com>
2020-09-11 13:20:30 +0700

    komen api, eror

--- routes/api.php
+++ routes/api.php
@@ -18,9 +18,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/product', function () {
-    return response->json(
-        [
-            "message" -> "method get sukses"
-        ]);
-});
+//Route::get('/product', function () {
+//    return response->json(
+//        [
+//            "message" -> "method get sukses"
+//        ]);
+//});
